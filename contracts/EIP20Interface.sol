d6e5844d4a741fb7a780d632d0fd286a48d6eaa1
Travis Goff <travisjerome5@gmail.com>
2022-09-20 21:23:33 -0400

    vxli

--- contracts/EIP20Interface.sol
+++ contracts/EIP20Interface.sol
@@ -48,3 +48,4 @@ contract EIP20Interface {
     event Transfer(address indexed _from, address indexed _to, uint256 _value); 
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
+
