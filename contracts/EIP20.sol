26f18f4c486075d0a632238ea2ac3650996e9afa
Travis Goff <travisjerome5@gmail.com>
2022-10-02 17:42:34 -0400

    versor

--- contracts/EIP20.sol
+++ contracts/EIP20.sol
@@ -3,7 +3,7 @@ Implements EIP20 token standard: https://github.com/ethereum/EIPs/issues/20
 .*/
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.18;
+pragma solidity ^0.8.17;
 
 import "./EIP20Interface.sol";
 
